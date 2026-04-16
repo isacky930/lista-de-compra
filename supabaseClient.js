77d90a056937698fe85139f1f5ef75ec4616b845
isacky930 <isacky930@gmail.com>
2026-04-16 12:40:30 -0300

    ok

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,5 +2,5 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 export const supabase = createClient(
   'https://fdcrqqfrshcqckypgips.supabase.co',
-  'sb_publishable_COU2ksqqsEK1CfUxu4pE4g_dIpPSdCF'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4dWdtaG1tbnZvYmNuemtxeWd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMTE1NzIsImV4cCI6MjA4NzY4NzU3Mn0.jRmwGRlHik1oActu6K07qpLRaLzFL0JNlnoRyYhAtDQ'
 )
